33b5e725120015821526ffdffcecdfb0a54aabfe
Frederik <132744263+FrederikNorlyk@users.noreply.github.com>
2023-07-20 14:50:35 +0000

    eslint

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { resolve } from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
